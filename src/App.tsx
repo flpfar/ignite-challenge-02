a0ecd8d961be9a28144aa5c3765cf288d3fd8d5a
Felipe Augusto Rosa <flp.far@gmail.com>
2021-03-15 21:26:52 -0300

    Add SideBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
 
-// import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
 
 import { api } from './services/api';
 
 import './styles/global.scss';
-
-import './styles/sidebar.scss';
-
 interface GenreResponseProps {
   id: number;
   name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
@@ -57,22 +53,7 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              id={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
+      <SideBar genres={genres} handleClickButton={handleClickButton} selectedGenreId={selectedGenreId} />
 
       <Content selectedGenre={selectedGenre} movies={movies} />
     </div>
